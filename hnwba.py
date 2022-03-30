c3cbce66062cd55fa549716c166415d646db8a43
sanjida9999 <101853629+sanjida9999@users.noreply.github.com>
2022-03-30 19:18:08 +0600

    Add files via upload

---
+++ hnwba.py
@@ -0,0 +1,203 @@
+<!-- Created by 🌟Vaishnavi Kumari🌟[Inactive] -->
+
+<!--    
+     
+    🎇🌷🌼💐🌺🌸🏵️🏵️🌻🌻🌸🌺🌺🌸🏵️🏵️🌻🌻🌸🌺🌷🌼💐🎇
+    
+    🎇 ★ Created by 🌟Vaishnavi Kumari🌟          🎇
+
+    🎇 ★ Do not copy ❌ without permission        🎇
+          
+    🎇 ★ Creation date : 28st March 2022          🎇 
+    
+    🎇 ★ Upvote and $hare if you like             🎇
+    
+    🎇 ★ 𝗧𝗵𝗮𝗻𝗸 𝘆𝗼𝘂 di for being their for me      🎇
+    
+    🎇🌷🌼💐🌺🌸🏵️🏵️🌻🌻🌸🌺🌺🌸🏵️🏵️🌻🌻🌸🌺🌷🌼💐🎇
+    
+ -->
+ 
+ 
+ <!--
+ ┌──❀*̥˚──◌───◌───◌──❀*̥˚─◌┐
+ 
+        Created by 
+       your lil sis
+        
+       I could have made 
+     it a lot better but
+       due to lack of 
+      time & board exam 
+      pressure I couldn't
+     make it as special
+        as i planned.
+      
+      the same thing
+      had happened in 
+     the case of somu di
+     (it was even worse
+       i couldnt even 
+      make her a card
+      and I really 
+      feel bad about
+      it) her bday 
+      was during
+      (my 5th or 6th 
+      subject)boards
+      exam day when 
+       I had my my 
+    english or physics
+           paper.
+  
+    i know somu di dosent 
+    mind these lil things 
+    but i felt really bad 
+     coz she had always 
+    suported me and made 
+      me laugh just like 
+       Aru(Arohi)di..
+  
+     i didnt want to 
+     make the same mistake
+     again. And sorry Somu 
+     di I couldn't have got 
+     a better opportunity
+     to apoloize ...
+     
+     Now this looks like
+     some heartfelt 
+     speech so I would 
+     conclude by saying
+ 
+     THANK YOU DIDI(S) for 
+         being there
+          by my side  
+          
+ └◌───❀*̥˚─◌───◌───◌───❀*̥˚┘
+          
+-->
+
+ 
+ 
+ <!-- ★ Hover the image ★ -->
+
+ 
+ 
+<!-- The print on the card is inspired from a whatsapp clone (on SL)  -->
+ 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Happy birthday Di</title>
+    
+        
+          <meta name="viewport" content="width=device-width, initial-scale=1"/><script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    </head>
+    <body>
+    <script>
+        
+    Swal.fire({
+  title: 'Arohi di🎇',
+  text: 'Happy Dirthday Di.',
+  imageUrl: 'https://www.mynameart.com/user-upload/6241a55abbc86.jpg',
+  imageWidth: 190,
+  imageHeight: 180,
+  imageAlt: 'Custom image',
+})
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.sound.min.js"></script>
+
+        <div class="card">
+            <div class="line">
+              <div class="profile-img" id='img'>
+    <img src="https://api.sololearn.com/Uploads/Avatars/20095418.jpg">
+</div>
+
+            
+   <!-- <h1>🎁Gift🎁</h1>
+   
+  🤥🤫🤐
+   https://drive.google.com/file/d/10CwH96SQVsrSGC7wadA8nvNLKMj9GPfB/view?usp=drivesdk
+   
+   
+
+
+★★★★curved design★★★
+
+<div id="curved-corner-bottomright">
+
+<div id="curved-corner-topleft">
+
+<style>
+
+            #curved-corner-bottomright,
+#curved-corner-topleft {
+  width: 160px;
+  height: 160px;
+  overflow: hidden;
+  position: relative;
+}
+#curved-corner-bottomright:before,
+#curved-corner-topleft:before
+ {
+  content: "";
+  display: block;
+  width: 200%;
+  height: 200%;
+  position: absolute;
+  border-radius: 50%;
+}
+#curved-corner-bottomright:before {
+  bottom: 0;
+  right: 0;
+  box-shadow: 50px 50px 0 0 red;
+}
+#curved-corner-topleft:before {
+  top: 0;
+  left: 0;
+  box-shadow: -50px -50px 0 0 red;
+}
+
+
+</style>
+             
+     -->   
+     
+        
+      <p class="wishes">
+            It’s crazy how good of a friend & sis you are,
+but it’s even crazier how nutty you are!
+It's so fun to live inside your head,
+and you will always be my sis until I'm dead.
+Wowww it rhymes 😂🤣
+
+May your birthday this
+year be just as wild and wonderful,
+and as hysterically funny as you are!<br>
+
+You couldn’t be any more wild and
+wonderful than you already are, and
+I wouldn’t have you any other way!
+
+My best wishes for the didi who says 
+and does crazy things.............
+I know I'm going to get beaten for
+always spilling the beans.🤭🤫🤥🤐
+        </p>
+        </div>
+
+     
+            <a id="follow-link" href=" https://www.sololearn.com/Profile/20095418/?ref=app" ><button id="follow">Wish Her🎉</button></a>
+        
+        
+    </div>
+    
+
+    
+    
+    
+    </body>
+</html>
